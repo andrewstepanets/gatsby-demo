be754c3aba57589219ad3c990932b4fcbb7a35cf
Andrii Stepanets <ovrass@gmail.com>
2020-09-24 19:10:34 +0300

    Correct issue

--- src/pages/products.js
+++ src/pages/products.js
@@ -42,7 +42,7 @@ export const query = graphql`
                 price
                 image {
                     fluid(maxWidth: 400) {
-                        ...GatsbyContentfulFluid_tracedSVG
+                        ...GatsbyContentfulFluid
                     }
                 }
             }
